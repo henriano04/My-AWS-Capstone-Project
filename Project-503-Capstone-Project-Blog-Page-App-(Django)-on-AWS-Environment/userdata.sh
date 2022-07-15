a45529b9fe89d58ca22138eda41b6ee3c754db53
henry <henriano04@gmail.com>
2022-07-15 14:56:19 -0400

    credential

--- Project-503-Capstone-Project-Blog-Page-App-(Django)-on-AWS-Environment/userdata.sh
+++ Project-503-Capstone-Project-Blog-Page-App-(Django)-on-AWS-Environment/userdata.sh
@@ -3,13 +3,13 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
-TOKEN=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-git clone https://$TOKEN@<YOUR GITHUB REPO URL>
-cd /home/ubuntu/<YOUR GITHUB REPO NAME>
+TOKEN="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"
+git clone https://$TOKEN@https://github.com/henriano04/My-AWS-Capstone-Project.git
+cd /home/ubuntu/My-AWS-Capstone-Project
 apt install python3-pip -y
 apt-get install python3.7-dev libmysqlclient-dev -y
 pip3 install -r requirements.txt
-cd /home/ubuntu/<YOUR GITHUB REPO NAME>/src
+cd /home/ubuntu/My-AWS-Capstone-Project/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
